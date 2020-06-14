fabbff63ce7b4d4e7429721958268b0c4d55f2bd
milankandel <milankandel9@gmail.com>
2020-06-14 18:49:51 +0545

    new commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Layout from './core/Layout';
+import { Helmet } from 'react-helmet';
+
+const App = () => {
+    const head = () => (
+        
+    );
+    return (
+        <Layout>
+            <div className="col-md-6 offset-md-3 text-center">
+                <h1 className="p-5">React Node MongoDB Authentication</h1>
+                <h2>MERN STACK</h2>
+                <hr />
+              
+             
+            </div>
+        </Layout>
+    );
+};
+
+export default App;
